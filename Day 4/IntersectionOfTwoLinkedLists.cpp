ed3ddbc0453e7cc9cb2de89f7fbc8477c870a801
vinay1212 <77916193+vinay1212@users.noreply.github.com>
2023-06-11 23:41:09 +0530

    Intersection Of Two Linked Lists

--- Day 4/IntersectionOfTwoLinkedLists.cpp
+++ Day 4/IntersectionOfTwoLinkedLists.cpp
@@ -113,4 +113,19 @@ Node* findIntersection(Node *firstHead, Node *secondHead)
 		}
 	}
 	return NULL;
+	
+	
+// 	if (firstHead == NULL || secondHead == NULL)
+//         return NULL;
+	
+//     Node* p1 = firstHead;
+//     Node* p2 = secondHead;
+	
+//     while (p1 != p2)
+//     {
+//         p1 = (p1 == NULL) ? secondHead : p1->next;
+
+//         p2 = (p2 == NULL) ? firstHead : p2->next;
+//     }
+//     return p1;
 }
