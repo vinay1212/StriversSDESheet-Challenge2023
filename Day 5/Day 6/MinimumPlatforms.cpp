917c8423d7d80796a8fd0c5a1cb3bfcbf222a2e0
vinay1212 <77916193+vinay1212@users.noreply.github.com>
2023-06-16 00:32:17 +0530

    Minimum Platforms

--- Day 5/Day 6/MinimumPlatforms.cpp
+++ Day 5/Day 6/MinimumPlatforms.cpp
@@ -1,3 +1,24 @@
+// ### Approach1 :
+
+// - first sort the trains according to there departure timing.
+// - for each train we will check how many overlapping trains exists.
+// - for each train check if its arrival time is before the departure time of current train —> increase the count, check if its greater than max.
+
+// - Time complexity : O( NlogN+N*N )
+// - space complexity : O(1)
+
+// ### Approach2 :
+
+// - we will keep track of arrival and departure of trains,
+// - create a vector pair, insert all arrival and departure times into it,  if entry is arrival mark it as arrival, same for departure
+// - now sort this vector pair according to times, some special cases (guess)
+// - now run a for loop on this vector pair, if it is arrival time increment count++, check if its greater than max, if its departure decrement count- -.
+
+// - Time complexity : O( NlogN+N )
+// - space complexity : O(N)
+
+
+
 bool sortcol(const pair<int,int> &v1, const pair<int,int> &v2){
     if(v1.first == v2.first){
         if(v1.second == 'a' && v2.second == 'd')
